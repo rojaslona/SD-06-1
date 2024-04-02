04093362bb1f02f81ca0c94bc5572f2abdfb2226
PranavKaushik123 <152179040+PranavKaushik123@users.noreply.github.com>
2024-04-02 12:54:05 +0530

    Delete task4.js

--- task4.js
+++
@@ -1,9 +0,0 @@
-// Type your code below this line!
-
-
-
-// Type your code above this line!
-
-const travel = new Journey(from, to)
-
-console.log("Booking a taxi from " + travel.start + " to " + travel.end + ".")
